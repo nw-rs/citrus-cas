2ebd419e8fd37ad878427fcbdcf95a1bfbae1b9c
willemml <willem@leit.so>
2021-06-10 18:03:00 -0700

    float parsing, and single symbols

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 //#![no_std]
 
+extern crate alloc;
+
 #[macro_use]
 extern crate nom;
 
-use core::str::FromStr;
-
+use alloc::vec::Vec;
+use nom::multi::many0;
+use nom::number::complete::float;
 use nom::IResult;
-
-named!(decimal<&str, u32>,
-  map!(recognize!(many1!(one_of!("0123456789"))), |s:&str|
-    dbg!(u32::from_str_radix(dbg!(s), 10).unwrap()
-  )
-));
 
 #[derive(Debug)]
 pub enum Token {
@@ -22,41 +19,19 @@ pub enum Token {
     Number(f32),
 }
 
-named!(float<&str, f32>,
-  alt!(
-    // Case one: .42
-    map_res!(
-      recognize!(tuple!(char!('.'),
-        decimal,
-        opt!(tuple!(one_of!("eE"), opt!(one_of!("+-")), decimal))
-      )), |s:&str| {
-        f32::from_str(s)
-      }
+named!(math_token<&str, Token>,
+    alt!(
+        float => { |f| Token::Number(f) } |
+        tag!("*") => { |_| Token::Multiply } |
+        tag!("+") => { |_| Token::Add} |
+        tag!("-") => { |_| Token::Subtract} |
+        tag!("/") => { |_| Token::Divide}
     )
-  | // Case two: 42e42 and 42.42e42
-    map_res!(
-      recognize!(tuple!(
-        decimal,
-        opt!(preceded!(char!('.'), decimal)),
-        tuple!(one_of!("eE"), opt!(one_of!("+-")), decimal)
-      )), |s:&str| {
-        f32::from_str(s)
-      }
-    )
-  | // Case three: 42. and 42.42
-    map_res!(
-      recognize!(tuple!(
-        decimal,
-        char!('.'),
-        opt!(decimal)
-      )), |s:&str| {
-        f32::from_str(s)
-      }
-    )
-  | // Case four: 42
-    map_res!(decimal, |u: u32| {Ok::<_, nom::Err<nom::error::ErrorKind>>(u as f32)}
-)));
+);
 
-pub fn test(input: &str) -> IResult<&str, f32> {
-    float(dbg!(input))
+named!(math_expr<&str, Vec<Token>>, many0!(math_token));
+
+pub fn test(input: &str) -> IResult<&str, Token> {
+    //math_expr(dbg!(input))
+    math_token(input)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    println!("parse ouput: {:?}", rcas::test("12.2"));
+    println!("parse ouput: {:?}", rcas::test("21e21"));
+    println!("parse ouput: {:?}", rcas::test("21.21"));
+    println!("parse ouput: {:?}", rcas::test("21"));
+    println!("parse ouput: {:?}", rcas::test("21+2"));
+    println!("parse ouput: {:?}", rcas::test("21*2"));
     println!("Hello, world!");
 }
