d6f8b7221a1908198f3e7dcf6173917247fd5f94
MixedMatched <30960626+MixedMatched@users.noreply.github.com>
2022-08-02 01:17:00 -0400

    finalize recursive descent and syntax tree

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,10 @@
 
 extern crate alloc;
 
-mod expression;
 mod parser;
-mod expression_map;
 mod expression_tree;
-/*
-pub use expression::{Approx, Expression};
-pub use expression_map::{ExpressionMap, VariableMap,UserFunctionMap,};
-pub use parser::approx as parse_approximation;
-pub use parser::math_expr as parse_math_expression;
-pub use parser::eval as parse_evaluation;
-*/
+mod modifier;
+
 #[derive(Debug)]
 pub enum Error {
     NotEnoughMemory,
