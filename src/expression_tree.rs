248ce5d23bf112926d0f3f247e116ab38cdf0c3f
MixedMatched <30960626+MixedMatched@users.noreply.github.com>
2022-10-02 15:03:39 -0400

    fix percent

--- src/expression_tree.rs
+++ src/expression_tree.rs
@@ -393,6 +393,7 @@ impl fmt::Display for Expression {
             
             Expression::Negate(e) => write!(f, "-({})", e),
             Expression::Factorial(e) => write!(f, "({})!", e),
+            Expression::Percent(e) => write!(f, "({})%", e),
             
             Expression::Add(l, r) => write!(f, "({} + {})", l, r),
             Expression::Subtract(l, r) => write!(f, "({} - {})", l, r),
